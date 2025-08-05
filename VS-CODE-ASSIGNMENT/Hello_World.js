c1efb89fc390687187d6c95c5e0e4665cfa2d305
micheallaird <micheallaird@yahoo.com>
2025-08-04 20:26:24 -0600

    Fix spacing in variable declaration for consistency

--- VS-CODE-ASSIGNMENT/Hello_World.js
+++ VS-CODE-ASSIGNMENT/Hello_World.js
@@ -1,7 +1,7 @@
 var greeting = 'Hello, World!';
 console.log(greeting);
 
-var greeting2 ='Welcome to node.js';
+var greeting2 = 'Welcome to node.js';
 console.log(greeting2);
 
 var sentence1 = 'Learning how to debug code with the debugger';
